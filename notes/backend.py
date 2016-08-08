6e0cc39bab9e5ddf465af0074a18351286cb23f2
plahol <placeholderregistry@gmail.com>
2016-08-08 23:31:04 +0200

    Worked on backend.py

--- notes/backend.py
+++ notes/backend.py
@@ -25,19 +25,52 @@ class Note(object):
         
 class Backend(object):
     
-    def __init__(self):
+    """def __init__(self):
         self.notes = []
         #if sys.platform == "linux"
         current_working_directory = os.getcwd()
         for file in os.listdir(current_working_directory):
             if file.endswith(".note"):
-                self.notes.append(Note(file, True))
+                self.notes.append(Note(file, True))"""
                 
-    def add_note(self):
-        self.notes.append(Note(note_name))
+    def __init__(self):
+        try:
+            note_order = open(note_order.note, "r+")
+        except FileNotFoundError:
+            note_order = open(note_order.note, "w+")
+            note_order.write("pinned_notes = \n\nnormalnotes = \n"            
+        lines = note_order.readlines()
+        self.pinned = lines[1].split()
+        self.normal = lines[3].split()
         
+        self.pinned_notes = []
+        self.normal_notes = []
+        for file_name in pinned:
+            self.pinned_notes.append(Note(file_name, True))
+        for file_name in normal:
+            self.normal_notes.append(Note(file_name, True))
+            
+    def save_note_order(self):
+        pinned_string = " ".join(pinned)
+        normal_string = " ".join(pinned)
+        with note_order as open(note_order.note, "r+"):
+            note_order.write("""
+                pinned_notes = 
+                %s
+                normalnotes = 
+                %s
+                """ % (pinned_string, normal_string)              
+                                               
+    def add_note(self, file_name):
+        self.notes.append(Note(file_name))
+        self.normal.append(file_name)
+        self.save_note_order()
+                
     def remove_note(self, note_name):
         pass
+        
+    def change_order(self):
+        pass       
         
     def read_from_note(self, note_name):
         pass    
