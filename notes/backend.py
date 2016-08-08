b90061392bb578178330479342932ced48a226cd
plahol <placeholderregistry@gmail.com>
2016-08-09 00:10:51 +0200

    backend interprets, doesn't check for existence of note when doing add_note()

--- notes/backend.py
+++ notes/backend.py
@@ -5,7 +5,7 @@ class Note(object):
     
     def __init__(self, file_name, existence):
         if existence == True:
-            self.file = open(file_name, "r+")
+            self.file = open(file_name + ".note", "r+")
         else:
             self.file = open(file_name + ".note", "w+")
         
@@ -35,35 +35,34 @@ class Backend(object):
                 
     def __init__(self):
         try:
-            note_order = open(note_order.note, "r+")
+            note_order = open("note_order.note", "r+")
+            lines = note_order.readlines()
+            print(lines)
+            self.pinned = lines[1].split()
+            self.normal = lines[3].split()
         except FileNotFoundError:
-            note_order = open(note_order.note, "w+")
-            note_order.write("pinned_notes = \n\nnormalnotes = \n"            
-        lines = note_order.readlines()
-        self.pinned = lines[1].split()
-        self.normal = lines[3].split()
+            note_order = open("note_order.note", "w+")
+            note_order.write("pinned_notes = \n\nnormal_notes = \n\n")         
+            self.pinned = []
+            self.normal = []
         
         self.pinned_notes = []
         self.normal_notes = []
-        for file_name in pinned:
+        for file_name in self.pinned:
             self.pinned_notes.append(Note(file_name, True))
-        for file_name in normal:
+        for file_name in self.normal:
             self.normal_notes.append(Note(file_name, True))
             
     def save_note_order(self):
-        pinned_string = " ".join(pinned)
-        normal_string = " ".join(pinned)
-        with note_order as open(note_order.note, "r+"):
-            note_order.write("""
-                pinned_notes = 
-                %s
-                normalnotes = 
-                %s
-                """ % (pinned_string, normal_string)              
+        pinned_string = " ".join(self.pinned)
+        normal_string = " ".join(self.normal)
+        with open("note_order.note", "r+") as note_order:
+            test = "pinned notes = \n%s\nnormal_notes = \n%s" % (pinned_string, normal_string) 
+            note_order.write(test)             
                                                
     def add_note(self, file_name):
-        self.notes.append(Note(file_name))
         self.normal.append(file_name)
+        self.normal_notes.append(Note(file_name, False))
         self.save_note_order()
                 
     def remove_note(self, note_name):
@@ -79,6 +78,7 @@ class Backend(object):
         pass            
                         
 backend = Backend()
+backend.add_note("01")
                        
 
                                 
