cc66cee3ed2b6f0408faca25ac9e3639728d9556
plahol <placeholderregistry@gmail.com>
2016-10-04 15:51:07 +0200

    GUI has minimal size, scales, adding cards works

--- notes/gui_tkinter.py
+++ notes/gui_tkinter.py
@@ -11,11 +11,14 @@ class Gui(object):
         self.selector = Selector(self.root)
         self.editor = Editor(self.root)
         
-        self.root.rowconfigure(0, weight=1)
-        self.root.columnconfigure(0, weight=1)
+        # with minsize set and the widgets sticking to all borders
+        # the widget size doesn't actually matter
+        self.root.rowconfigure(0, weight=1, minsize=300)
+        self.root.columnconfigure(0, weight=2, minsize=300)
+        self.root.columnconfigure(1, weight=3, minsize=500)
         self.selector.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.editor.grid(row=0, column=1)
-        
+        self.editor.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
+
         self.root.mainloop()
         
     
@@ -23,24 +26,24 @@ class Selector(tk.Frame):
     
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        self.config(background="blue")
         
         self.toolbar()
-        #card = CardFrame(self, 0)
-        #card.grid(row=1, column=0)
-        
-        self.rowconfigure(2, weight=1)
-        self.columnconfigure(0, weight=1)
-        card2 = Card(self, 0, 100, 100)
-        card2.grid(row=2, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-        self.rowconfigure(3, weight=1)
-        card3 = Card(self, 1, 100, 100)
-        card3.grid(row=3, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
-
+        self.cards = []
         
     def toolbar(self):
-        add_note = tk.Button(self, text="Add Note")
-        add_note.grid(row=0, column=0)
+        button_add_note = tk.Button(self, text="Add Note", command=self.add_note)
+        # button isn't centred until a note is added, why?
+        button_add_note.grid(row=0, column=0)
+
+    def add_note(self):
+        self.cards.append(Card(self, "placeholder", 0, 0))
+        index_last = len(self.cards)
+        # first row is occupied by button_add_note, hence note x occupying row x + 1
+        self.cards[-1].grid(row=index_last, column=0, 
+                            sticky=tk.N+tk.S+tk.E+tk.W)
+        self.rowconfigure(index_last, weight=1)
+        self.columnconfigure(0, weight=1)
+
         
     def cards(self):
         pass
@@ -52,13 +55,18 @@ class Editor(tk.Frame):
     
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
+        self.textfield()
         
     def toolbar(self):
         pass
         
     def textfield(self):
-        pass
+        textfield = tk.Text(self)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        textfield.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
         
+
 class Card(tk.Canvas):
 
     def __init__(self, parent, note_name, width, height): 
@@ -71,22 +79,23 @@ class Card(tk.Canvas):
         
         self.note_name = note_name
         
+        self.button_size = 30 
         self.icon_close = tk.PhotoImage(file="button.gif")
         self.remove_note = tk.Button(self,
                                      image=self.icon_close,
-                                     width=40, height=40)
+                                     width=self.button_size, height=self.button_size)
         
         self.pin_note = tk.Button(self,
                                   image=self.icon_close,
-                                  width=40, height=40)
+                                  width=self.button_size, height=self.button_size)
         self.window_remove = self.create_window((width, 0),
                                                 window=self.remove_note,
                                                 anchor=tk.NE)
-        self.window_pin = self.create_window((width, 40),
+        self.window_pin = self.create_window((width, self.button_size),
                                              window=self.pin_note,
                                              anchor=tk.NE)    
         self.preview_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        preview_width = width - 40
+        preview_width = width - self.button_size
         #for whatever reason self.cget("width") is a String
         #whereas             remove_note.cget("width") is an Int
         self.preview = self.create_text((0,0), 
@@ -105,10 +114,11 @@ class Card(tk.Canvas):
         self.create_window((self.winfo_width(), 0),
                            window=self.remove_note,
                            anchor=tk.NE)
-        self.create_window((self.winfo_width(), 40),
+        self.create_window((self.winfo_width(), self.button_size),
                            window=self.pin_note,
                            anchor=tk.NE)
-        preview_width = self.winfo_width() - 40 
+        # needs to be tested for correct preview_width
+        preview_width = self.winfo_width() - 50 
         self.itemconfig(self.preview, 
                         width=preview_width)
 
